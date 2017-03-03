0b0f1b56f66a7af3f64ca37fd98e61912f24f019
Wooseok Song <bboyskip115@gmail.com>
2017-03-04 01:33:03 +0900

    1. make login activity design. 2. make register activity design.

---
+++ GrooveJunkiesApp/app/src/main/java/com/example/wooseoksong/groovejunkiesapp/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.example.wooseoksong.groovejunkiesapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wooseoksong on 2017. 3. 3..
+ */
+
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.register_layout);
+    }
+}
